7dbe9279b7875c9bbafe127fe049df3ea5614909
maxb-hystax <24920606+maxb-hystax@users.noreply.github.com>
2023-08-17 06:54:04 +0000

    OS-6583: moved cloud_adapter to tools
    * Moved cloud_adapter

---
+++ tools/cloud_adapter/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+requirements = [
+    # AWS
+    'boto3==1.12.25',
+
+    # Alibaba Cloud
+    'aliyun-python-sdk-ecs==4.24.1',
+    'aliyun-python-sdk-core-v3==2.13.32',
+    'aliyun-python-sdk-ram==3.2.0',
+    'aliyun-python-sdk-sts==3.0.2',
+    'aliyun-python-sdk-bssopenapi==1.6.8',
+    'aliyun-python-sdk-cms==7.0.22',
+    'aliyun-python-sdk-rds==2.5.11',
+    'aliyun-python-sdk-vpc==3.0.14',
+
+    # Azure
+    'azure-mgmt-resource==2.1.0',
+    'azure-mgmt-network==2.6.0',
+    'azure-mgmt-compute==7.0.0',
+    "azure-storage-blob==1.5.0",
+    'azure-mgmt-storage==3.3.0',
+    "azure_mgmt_subscription==0.2.0",
+    "azure-mgmt-consumption==8.0.0",
+    "azure-mgmt-monitor==0.10.0",
+    "azure-mgmt-commerce==1.0.1",
+    "msrest==0.6.6",
+    "urllib3==1.25.3",
+    "azure-identity==1.6.1",
+
+    # Gcp
+    'google-cloud-bigquery==3.0.1',
+    'google-cloud-compute==1.3.0',
+    'google-cloud-storage==2.3.0',
+    'google-cloud-monitoring==2.9.1',
+
+    # Nebius
+    'yandexcloud==0.191.0',
+
+    "requests==2.25.0",
+    "retrying==1.3.3",
+]
+
+setup(name='cloud-adapter',
+      description='OptScale Cloud adapter',
+      url='http://hystax.com',
+      author='Hystax',
+      author_email='info@hystax.com',
+      package_dir={'cloud_adapter': ''},
+      packages=['cloud_adapter', 'cloud_adapter.clouds', 'cloud_adapter.lib',
+                'cloud_adapter.lib.azure_partner'],
+      install_requires=requirements,
+      )
