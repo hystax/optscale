9fb5ff063d7765bf36658436442abcb58bd11de7
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2023-09-22 11:16:48 +0400

    OS-6727. Added sorting for recommendations
    Sort by savings and if there are no savings - sort by count

--- ngui/ui/src/containers/RecommendationsOverviewContainer/RecommendationsOverviewContainerMocked.js
+++ ngui/ui/src/containers/RecommendationsOverviewContainer/RecommendationsOverviewContainerMocked.js
@@ -3,40 +3,36 @@ import { ALL_SERVICES } from "hooks/useRecommendationServices";
 import { DEFAULT_RECOMMENDATIONS_FILTER, DEFAULT_VIEW } from "./Filters";
 import { MOCKED_DATA } from "./mockedData";
 import { ALL_RECOMMENDATIONS } from "./recommendations/allRecommendations";
-import { ACTIVE } from "./recommendations/BaseRecommendation";
 import RecommendationsOverview from "./RecommendationsOverview";
 
 const fn = () => {};
-const RecommendationsOverviewContainerMocked = () => {
-  const recommendations = Object.values(ALL_RECOMMENDATIONS).map(
-    (RecommendationClass) => new RecommendationClass(ACTIVE, MOCKED_DATA)
-  );
 
-  return (
-    <RecommendationsOverview
-      data={MOCKED_DATA}
-      isDataReady
-      onRecommendationClick={fn}
-      setCategory={fn}
-      category={DEFAULT_RECOMMENDATIONS_FILTER}
-      setSearch={fn}
-      search=""
-      setView={fn}
-      view={DEFAULT_VIEW}
-      setService={fn}
-      service={ALL_SERVICES}
-      recommendations={recommendations}
-      downloadLimit={0}
-      riSpExpensesSummary={{
-        totalCostWithOffer: 208.83945818440003,
-        totalSaving: 44.93673360120001,
-        computeExpensesCoveredWithCommitments: 0.07
-      }}
-      isRiSpExpensesSummaryLoading={false}
-    />
-  );
-};
-
-RecommendationsOverviewContainerMocked.propTypes = {};
+const RecommendationsOverviewContainerMocked = () => (
+  <RecommendationsOverview
+    lastCompleted={MOCKED_DATA.last_completed}
+    totalSaving={MOCKED_DATA.total_saving}
+    nextRun={MOCKED_DATA.next_run}
+    lastRun={MOCKED_DATA.last_run}
+    isDataReady
+    onRecommendationClick={fn}
+    setCategory={fn}
+    category={DEFAULT_RECOMMENDATIONS_FILTER}
+    setSearch={fn}
+    search=""
+    setView={fn}
+    view={DEFAULT_VIEW}
+    setService={fn}
+    service={ALL_SERVICES}
+    recommendationsData={MOCKED_DATA}
+    recommendationClasses={ALL_RECOMMENDATIONS}
+    downloadLimit={0}
+    riSpExpensesSummary={{
+      totalCostWithOffer: 208.83945818440003,
+      totalSaving: 44.93673360120001,
+      computeExpensesCoveredWithCommitments: 0.07
+    }}
+    isRiSpExpensesSummaryLoading={false}
+  />
+);
 
 export default RecommendationsOverviewContainerMocked;
