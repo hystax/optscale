99eefeb07aa5a1be61a26f4e0aafbb1a89758e9a
ab-hystax <142571159+ab-hystax@users.noreply.github.com>
2023-09-27 11:12:27 +0300

    OS-5749: [Charts] Tooltip "jumpy" behaviour

--- ngui/ui/src/components/ChartTooltip/ChartTooltip.styles.js
+++ ngui/ui/src/components/ChartTooltip/ChartTooltip.styles.js
@@ -1,3 +1,4 @@
+import { keyframes } from "tss-react";
 import { makeStyles } from "tss-react/mui";
 
 const useStyles = makeStyles()((theme) => ({
@@ -8,7 +9,17 @@ const useStyles = makeStyles()((theme) => ({
     paddingTop: theme.spacing(0.5),
     paddingBottom: theme.spacing(0.5),
     paddingLeft: theme.spacing(1),
-    paddingRight: theme.spacing(1)
+    paddingRight: theme.spacing(1),
+    // styles below is a workaround https://github.com/plouc/nivo/issues/2161
+    opacity: 0,
+    animation: `${keyframes`
+            0%, 10% {
+              opacity: 0;
+            }
+            100% {
+              opacity: 1;
+            }
+            `} 0.1s normal forwards ease-out`
   }
 }));
 
