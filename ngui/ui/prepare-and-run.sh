205320c932285aaa544c3f290e4839fa19dcb96f
v-hx <146182370+v-hx@users.noreply.github.com>
2023-11-14 13:41:18 +0300

    OS-6782: Added site indexing in production

--- ngui/ui/prepare-and-run.sh
+++ ngui/ui/prepare-and-run.sh
@@ -9,11 +9,25 @@ function initializeEnvironmentVariables(){
         echo "window.optscale.${key}='${val}' ;";
     done
 }
+
+function allowAppIndexing(){
+    ROBOTS_TXT_PATH=/usr/src/app/ui/public/robots.txt
+    > $ROBOTS_TXT_PATH
+    {
+        echo "User-agent: *"
+        echo "Allow: /"
+    } >> $ROBOTS_TXT_PATH
+}
+
 initializeEnvironmentVariables > /usr/src/app/ui/build/config.js
 
 # updating script include version in html file, to drop client browser cache
 current_timestamp=`date +%s`
 sed -i /usr/src/app/ui/build/index.html -e "s/\${buildVersion}/${current_timestamp}/" /usr/src/app/ui/build/index.html
+
+if [ "$BUILD_MODE" = "production" ]; then
+   allowAppIndexing
+fi
 
 # running application
 node ./dist/server.js
