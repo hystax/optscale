0f57336e778883545a08154c2dd9edb52ac33209
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2023-12-28 11:45:25 +0400

    OS-7202. Cover SetupLeaderboard page with EDIT_PARTNER permission

--- ngui/ui/src/components/Protector/Protector.tsx
+++ ngui/ui/src/components/Protector/Protector.tsx
@@ -1,7 +1,13 @@
+import { type ReactNode } from "react";
 import Error from "components/Error";
 import { useIsAllowed } from "hooks/useAllowedActions";
 
-const Protector = ({ children, allowedActions = [] }) => {
+type ProtectorProps = {
+  children: ReactNode;
+  allowedActions: string[];
+};
+
+const Protector = ({ children, allowedActions = [] }: ProtectorProps) => {
   const shouldRenderChildren = useIsAllowed({ requiredActions: allowedActions });
 
   return shouldRenderChildren ? children : <Error messageId="forbidden" />;
