40d711e94064b6b20f329a1afb8fd00775406f6f
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2024-01-11 14:36:31 +0400

    OS-7215. Request ml recommendations in the live demo
    Removed check for `isDemo` and removed corresponding "demo" container

--- ngui/ui/src/containers/MlModelRecommendationsContainer/MlModelRecommendationsContainer.tsx
+++ ngui/ui/src/containers/MlModelRecommendationsContainer/MlModelRecommendationsContainer.tsx
@@ -1,26 +1,14 @@
 import { useParams } from "react-router-dom";
 import MlModelRecommendations from "components/MlModelRecommendations";
-import { useOrganizationInfo } from "hooks/useOrganizationInfo";
 import MlModelsService from "services/MlModelsService";
 
-const DemoContainer = () => {
-  const { taskId } = useParams();
-
-  return <MlModelRecommendations taskId={taskId} isLoading={false} recommendations={MlModelRecommendations} />;
-};
-
-const Container = () => {
+const MlModelRecommendationsContainer = () => {
   const { taskId } = useParams();
 
   const { useGetModelRecommendations } = MlModelsService();
   const { isLoading: isGetRecommendationsLoading, recommendations } = useGetModelRecommendations(taskId);
+
   return <MlModelRecommendations taskId={taskId} isLoading={isGetRecommendationsLoading} recommendations={recommendations} />;
-};
-
-const MlModelRecommendationsContainer = () => {
-  const { isDemo } = useOrganizationInfo();
-
-  return isDemo ? <DemoContainer /> : <Container />;
 };
 
 export default MlModelRecommendationsContainer;
