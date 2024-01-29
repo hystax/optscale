fdd3394eb1c76c2910f7c2f6121a08ab2dc20092
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2024-01-29 16:02:23 +0400

    OS-7221: Remove support for the "ri_sp_enabled" feature flag
    This change ensures that the "ri/sp coverage" card is now available for all users.

--- ngui/ui/src/hooks/useIsRiSpEnabled.ts
+++
@@ -1,9 +0,0 @@
-import { useOrganizationInfo } from "hooks/useOrganizationInfo";
-import { useIsFeatureEnabled } from "./useIsFeatureEnabled";
-
-export const useIsRiSpEnabled = () => {
-  const isFeatureEnabled = useIsFeatureEnabled("ri_sp_enabled");
-  const { isDemo } = useOrganizationInfo();
-
-  return isFeatureEnabled || isDemo;
-};
