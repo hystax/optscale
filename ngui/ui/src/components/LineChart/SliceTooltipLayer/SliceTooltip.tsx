7f5b9a76d3ab0a4fb4311b6cf1b3f22b1ba8715d
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2024-03-07 14:00:02 +0400

    OS-7352. Update line chart tooltip width setting and position calculation
    Make sure that the tooltip is always displayed in the chart area

--- ngui/ui/src/components/LineChart/SliceTooltipLayer/SliceTooltip.tsx
+++ ngui/ui/src/components/LineChart/SliceTooltipLayer/SliceTooltip.tsx
@@ -1,39 +1,82 @@
+import { ReactNode } from "react";
 import { useMeasure } from "@nivo/core";
-
-const TOOLTIP_OFFSET = 14;
 
 export const TOOLTIP_ANCHOR = Object.freeze({
   LEFT: "left",
   RIGHT: "right"
 });
 
-const SliceTooltip = ({ position, anchor, children }) => {
-  const [measureRef, bounds] = useMeasure();
+type SliceTooltipProps = {
+  chartWrapperWidth: number;
+  mousePosition: { x: number; y: number };
+  anchor: (typeof TOOLTIP_ANCHOR)[keyof typeof TOOLTIP_ANCHOR];
+  children: ReactNode;
+};
 
-  let x = Math.round(position.x);
-  let y = Math.round(position.y);
+const TOOLTIP_OFFSET = 14;
 
-  const hasDimension = bounds.width > 0 && bounds.height > 0;
+const getTooltipTopLeftCornerCoordinates = (
+  mousePosition: SliceTooltipProps["mousePosition"],
+  tooltipBounds: ReturnType<typeof useMeasure>[1],
+  anchor: SliceTooltipProps["anchor"]
+) => {
+  let x = Math.round(mousePosition.x);
+  let y = Math.round(mousePosition.y);
+
+  const hasDimension = tooltipBounds.width > 0 && tooltipBounds.height > 0;
 
   if (hasDimension) {
     if (anchor === TOOLTIP_ANCHOR.RIGHT) {
       x += TOOLTIP_OFFSET;
-      y -= bounds.height / 2;
+      y -= tooltipBounds.height / 2;
     }
     if (anchor === TOOLTIP_ANCHOR.LEFT) {
-      x -= bounds.width + TOOLTIP_OFFSET;
-      y -= bounds.height / 2;
+      x -= tooltipBounds.width + TOOLTIP_OFFSET;
+      y -= tooltipBounds.height / 2;
     }
   }
 
+  return {
+    x,
+    y
+  };
+};
+
+const getXOffset = (xTopLeftCorner: number, tooltipWidth: number, chartWrapperWidth: number) => {
+  const isFitOnLeft = xTopLeftCorner > 0;
+  const isFitOnTheRight = xTopLeftCorner + tooltipWidth < chartWrapperWidth;
+
+  if (!isFitOnLeft) {
+    // move tooltip to the right back to the chart area
+    return -xTopLeftCorner;
+  }
+  if (!isFitOnTheRight) {
+    // move tooltip to the left back to the chart area
+    return chartWrapperWidth - (xTopLeftCorner + tooltipWidth);
+  }
+
+  return 0;
+};
+
+const SliceTooltip = ({ chartWrapperWidth, mousePosition, anchor, children }: SliceTooltipProps) => {
+  const [tooltipRef, tooltipBounds] = useMeasure();
+
+  const { x: tooltipTopLeftCornerXCoordinate, y: tooltipTopLeftCornerYCoordinate } = getTooltipTopLeftCornerCoordinates(
+    mousePosition,
+    tooltipBounds,
+    anchor
+  );
+
   return (
     <div
-      ref={measureRef}
+      ref={tooltipRef}
       style={{
         position: "absolute",
         pointerEvents: "none",
-        left: x,
-        top: y
+        left:
+          tooltipTopLeftCornerXCoordinate + getXOffset(tooltipTopLeftCornerXCoordinate, tooltipBounds.width, chartWrapperWidth),
+        top: tooltipTopLeftCornerYCoordinate,
+        maxWidth: chartWrapperWidth
       }}
     >
       {children}
