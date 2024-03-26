dd6a1eef6bca14bcccde3dca037d4223a0d09f9b
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2024-03-26 12:13:24 +0400

    OS-7423. Add Refresh button on Datasets page

--- ngui/ui/src/components/MlDatasets/MlDatasets.tsx
+++ ngui/ui/src/components/MlDatasets/MlDatasets.tsx
@@ -1,16 +1,31 @@
+import RefreshOutlinedIcon from "@mui/icons-material/RefreshOutlined";
 import { Stack } from "@mui/system";
+import { GET_ML_DATASETS } from "api/restapi/actionTypes";
 import ActionBar from "components/ActionBar";
 import MlDatasetsTable from "components/MlDatasetsTable";
 import PageContentWrapper from "components/PageContentWrapper";
 import TableLoader from "components/TableLoader";
+import { useRefetchApis } from "hooks/useRefetchApis";
 import { SPACING_2 } from "utils/layouts";
 
 const PageActionBar = () => {
+  const refetch = useRefetchApis();
+
   const actionBarDefinition = {
     title: {
       messageId: "datasets",
       dataTestId: "lbl_ml_datasets"
-    }
+    },
+    items: [
+      {
+        key: "btn-refresh",
+        icon: <RefreshOutlinedIcon fontSize="small" />,
+        messageId: "refresh",
+        dataTestId: "btn_refresh",
+        type: "button",
+        action: () => refetch([GET_ML_DATASETS])
+      }
+    ]
   };
 
   return <ActionBar data={actionBarDefinition} />;
