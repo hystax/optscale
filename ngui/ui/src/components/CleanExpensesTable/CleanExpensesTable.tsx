1fd975e9987b4fe805e90ba5a713951556fa3866
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2024-03-28 11:42:11 +0400

    OS-5775. Refactor ResourcePaidNetworkTrafficList to use ExpandableList component

--- ngui/ui/src/components/CleanExpensesTable/CleanExpensesTable.tsx
+++ ngui/ui/src/components/CleanExpensesTable/CleanExpensesTable.tsx
@@ -20,10 +20,11 @@ import { useApiState } from "hooks/useApiState";
 import { useOrganizationInfo } from "hooks/useOrganizationInfo";
 import { intl } from "translations/react-intl-config";
 import { getCreateAssignmentRuleUrl } from "urls";
+import { isEmpty as isEmptyArray } from "utils/arrays";
 import { resourcePoolOwner, tags } from "utils/columns";
 import { CLEAN_EXPENSES_TABLE_QUERY_PARAM_PREFIX, DOWNLOAD_FILE_FORMATS } from "utils/constants";
 import { MetadataNodes } from "utils/metadata";
-import { RESOURCE_ID_COLUMN_CELL_STYLE } from "utils/tables";
+import { CELL_EMPTY_VALUE, RESOURCE_ID_COLUMN_CELL_STYLE } from "utils/tables";
 import CollapsableTableCell from "../CollapsableTableCell";
 
 const LocationNodes = ({ region, service_name: serviceName, k8s_node: k8sNode, k8sNamespace }) => {
@@ -111,7 +112,12 @@ const CleanExpensesTable = ({
           row: {
             original: { traffic_expenses: trafficExpenses = [] }
           }
-        }) => <ResourcePaidNetworkTrafficList trafficExpenses={trafficExpenses} />
+        }) =>
+          isEmptyArray(trafficExpenses) ? (
+            CELL_EMPTY_VALUE
+          ) : (
+            <ResourcePaidNetworkTrafficList trafficExpenses={trafficExpenses} />
+          )
       },
       {
         header: (
--- ngui/ui/src/components/ResourcePaidNetworkTrafficList/ResourcePaidNetworkTrafficList.tsx
+++ ngui/ui/src/components/ResourcePaidNetworkTrafficList/ResourcePaidNetworkTrafficList.tsx
@@ -1,75 +1,46 @@
-import { Stack } from "@mui/material";
-import { FormattedMessage } from "react-intl";
-import DashedTypography from "components/DashedTypography";
+import ExpandableList from "components/ExpandableList";
 import FromToArrowLabel from "components/FromToArrowLabel";
 import KeyValueLabel from "components/KeyValueLabel/KeyValueLabel";
 import PaidNetworkTrafficLabel from "components/PaidNetworkTrafficLabel";
-import { useToggle } from "hooks/useToggle";
-import { sortObjects, isEmpty as isEmptyArray, splitIntoTwoChunks } from "utils/arrays";
-import { SPACING_1 } from "utils/layouts";
+import { sortObjects } from "utils/arrays";
 
 const EXPENSE_THRESHOLD = 1;
 
 const ELEMENTS_COUNT_LIMIT = 5;
 
-export const getTrafficExpensesDataChunks = (trafficExpenses) => {
-  if (isEmptyArray(trafficExpenses)) {
-    return {
-      expensesChunk1: [],
-      expensesChunk2: []
-    };
-  }
+const getMaxRows = (trafficExpensesSorted) => {
+  const thresholdIndex = trafficExpensesSorted.findIndex(({ cost }) => cost < EXPENSE_THRESHOLD);
 
+  return thresholdIndex !== -1 && thresholdIndex < ELEMENTS_COUNT_LIMIT
+    ? thresholdIndex || 1
+    : Math.min(trafficExpensesSorted.length, ELEMENTS_COUNT_LIMIT);
+};
+
+const ResourcePaidNetworkTrafficList = ({ trafficExpenses = [] }) => {
   const trafficExpensesSorted = sortObjects({
     array: trafficExpenses,
     field: "cost",
     type: "desc"
   });
 
-  const getSplitIndex = () => {
-    const thresholdIndex = trafficExpensesSorted.findIndex(({ cost }) => cost < EXPENSE_THRESHOLD);
-
-    return thresholdIndex !== -1 && thresholdIndex < ELEMENTS_COUNT_LIMIT
-      ? thresholdIndex || 1
-      : Math.min(trafficExpensesSorted.length, ELEMENTS_COUNT_LIMIT);
-  };
-
-  const splitIndex = getSplitIndex();
-
-  const [expensesChunk1, expensesChunk2] = splitIntoTwoChunks(trafficExpensesSorted, splitIndex);
-
-  return {
-    expensesChunk1,
-    expensesChunk2
-  };
-};
-
-const ResourcePaidNetworkTrafficList = ({ trafficExpenses = [] }) => {
-  const [isExpanded, toggle] = useToggle(false);
-
-  if (isEmptyArray(trafficExpenses)) {
-    return null;
-  }
-
-  const { expensesChunk1, expensesChunk2 } = getTrafficExpensesDataChunks(trafficExpenses);
-
   return (
-    <>
-      <Stack spacing={SPACING_1}>
-        {(isExpanded ? [...expensesChunk1, ...expensesChunk2] : expensesChunk1).map(({ from, to, cost, usage }) => (
+    <ExpandableList
+      items={trafficExpensesSorted}
+      render={(item) => {
+        const { from, to, cost, usage } = item;
+
+        return (
           <KeyValueLabel
             key={`${from}-${to}`}
             keyText={<FromToArrowLabel from={from} to={to} />}
             value={<PaidNetworkTrafficLabel cost={cost} usage={usage} />}
             isBoldValue={false}
+            gutterBottom
           />
-        ))}
-      </Stack>
-      <DashedTypography onClick={toggle}>
-        {!isExpanded && !isEmptyArray(expensesChunk2) && <FormattedMessage id="showMore" />}
-        {isExpanded && <FormattedMessage id="showLess" />}
-      </DashedTypography>
-    </>
+        );
+      }}
+      maxRows={getMaxRows(trafficExpensesSorted)}
+    />
   );
 };
 
