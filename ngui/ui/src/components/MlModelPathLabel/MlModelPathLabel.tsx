21be5f54b306043a32bc57009bbf15919b253c64
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2024-04-01 11:08:46 +0400

    OS-7439. Increase max number of characters in path field and add a copy button

--- ngui/ui/src/components/MlModelPathLabel/MlModelPathLabel.tsx
+++ ngui/ui/src/components/MlModelPathLabel/MlModelPathLabel.tsx
@@ -1,11 +1,16 @@
+import CopyText from "components/CopyText";
 import SlicedText from "components/SlicedText";
 
 type MlModelPathLabelProps = {
   path: string;
 };
 
-const PATH_LENGTH_LIMIT = 20;
+const PATH_LENGTH_LIMIT = 70;
 
-const MlModelPathLabel = ({ path }: MlModelPathLabelProps) => <SlicedText limit={PATH_LENGTH_LIMIT} text={path} />;
+const MlModelPathLabel = ({ path }: MlModelPathLabelProps) => (
+  <CopyText text={path}>
+    <SlicedText limit={PATH_LENGTH_LIMIT} text={path} />
+  </CopyText>
+);
 
 export default MlModelPathLabel;
