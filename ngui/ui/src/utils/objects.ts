4c4be3f51bd4b7e153dd8661a3c88dfa028fe26f
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2024-04-29 15:03:03 +0400

    OS-7458. [TS] Add types for object utils

--- ngui/ui/src/utils/objects.ts
+++ ngui/ui/src/utils/objects.ts
@@ -1,11 +1,22 @@
-export const isObject = (obj) => obj === Object(obj);
+export const isObject = (obj: unknown) => obj === Object(obj);
 
-export const idx = (keys, object, defaultValue = undefined) =>
-  keys.reduce((obj, key) => (obj && Object.prototype.hasOwnProperty.call(obj, key) ? obj[key] : defaultValue), object);
+export const idx = (keys: string[], object: Record<string, unknown>, defaultValue: unknown = undefined) =>
+  keys.reduce((obj: unknown, key: string): unknown => {
+    if (obj && Object.prototype.hasOwnProperty.call(obj, key)) {
+      return (obj as Record<string, unknown>)[key];
+    }
 
-export const isEmpty = (object) => Object.keys(object).length === 0 && object.constructor === Object;
+    return defaultValue;
+  }, object);
 
-export const filterEmpty = (sourceObject, options = {}) => {
+export const isEmpty = (object: Record<string, unknown>) => Object.keys(object).length === 0 && object.constructor === Object;
+
+export const filterEmpty = (
+  sourceObject: Record<string, unknown>,
+  options: {
+    allowEmptyString?: boolean;
+  } = {}
+): Record<string, unknown> => {
   const { allowEmptyString = false } = options;
 
   return Object.keys(sourceObject)
@@ -25,16 +36,12 @@ export const filterEmpty = (sourceObject, options = {}) => {
     );
 };
 
-export const removeKey = (object, key) => {
+export const removeKey = <T extends Record<string, unknown>, K extends keyof T>(object: T, key: K) => {
   const { [key]: removedKey, ...obj } = object;
   return obj;
 };
 
 /**
- *
- * @param {Object} object - object to sort
- * @param {"desc"|"DESC"|"asc"|"ASC"} order - sorting order
- *
  * @example
  * const obj = { a: 1, b: 3, c: 2 };
  * const sortD = sortByValue(obj, "desc")
@@ -42,34 +49,30 @@ export const removeKey = (object, key) => {
  *
  * const sortA = sortByValue(obj, "asc")
  * // sortA = { a: 1, c: 2, b: 3 }
- *
- * @returns {object}
  */
-export const sortByValue = (object, order = "asc") => {
-  if (!["asc", "desc"].includes(order.toLowerCase())) {
-    throw new Error("Incorrect 'order' value");
-  }
-
-  const ascSortFunction = (a, b) => a[1] - b[1];
-  const descSortFunction = (a, b) => b[1] - a[1];
+export const sortByValue = <T extends number>(object: Record<string, T>, order: "asc" | "desc" = "asc"): Record<string, T> => {
+  const ascSortFunction = (a: [string, T], b: [string, T]) => a[1] - b[1];
+  const descSortFunction = (a: [string, T], b: [string, T]) => b[1] - a[1];
 
   const sortFunction = {
     desc: descSortFunction,
     asc: ascSortFunction
-  }[order.toLowerCase()];
+  }[order];
 
   return Object.fromEntries(Object.entries(object).sort(sortFunction));
 };
 
-export const toKeyValueString = (object, separator = ":", joinWith = ",") =>
+export const toKeyValueString = (object: Record<string, unknown>, separator = ":", joinWith = ",") =>
   isEmpty(object)
     ? ""
     : Object.entries(object)
         .map(([key, value]) => `${key}${separator} ${value}`)
         .join(`${joinWith} `);
 
-export const objectMap = (object, mapFunction) =>
-  Object.fromEntries(Object.entries(object).map(([key, value], index) => [key, mapFunction(value, key, index)]));
+export const objectMap = <T extends Record<string, unknown>, V extends T[keyof T], K extends keyof T>(
+  object: T,
+  mapFunction: (value: V, key: K, index: number) => unknown
+) => Object.fromEntries(Object.entries(object).map(([key, value], index) => [key, mapFunction(value as V, key as K, index)]));
 
-export const removeUndefinedValues = (object) =>
+export const removeUndefinedValues = (object: Record<string, unknown>) =>
   Object.fromEntries(Object.entries(object).filter(([, value]) => value !== undefined));
