f198cb21bd7b74c053360c92f6f332ba0f7cdc00
Antonio Di Mariano <antonio.dimariano@gmail.com>
2024-10-17 09:30:57 +0200

    Add missing architecture arm64 support to the test suits.

--- rest_api/prepare_clickhouse_local.sh
+++ rest_api/prepare_clickhouse_local.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 set -e
-
-CLICKHOUSE_MAC_URL=macos
-CLICKHOUSE_LINUX_URL=amd64
-BINARIES=($CLICKHOUSE_LINUX_URL)
-OS_URL=$CLICKHOUSE_LINUX_URL
 CLICKHOUSE_ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    BINARIES+=($CLICKHOUSE_MAC_URL)
-    OS_URL=$CLICKHOUSE_MAC_URL
-fi
-for os in ${BINARIES[@]};
-do
-    CLICKHOUSE_FOLDER_PATH=$CLICKHOUSE_ROOT_DIR/.clickhouse/$os
-    CLICKHOUSE_BIN_PATH=$CLICKHOUSE_FOLDER_PATH/clickhouse
-    if [[ ! -e "$CLICKHOUSE_BIN_PATH" ]]; then
-        mkdir -p $CLICKHOUSE_FOLDER_PATH
-        tar_path=$CLICKHOUSE_BIN_PATH.tar.gz
-        cat $CLICKHOUSE_ROOT_DIR/rest_api_server/tests/binaries/clickhouse/$os/clickhouse.?? > $tar_path
-        tar -xzvf $tar_path -C $CLICKHOUSE_FOLDER_PATH
-        rm -rf $tar_path
-        chmod +x $CLICKHOUSE_BIN_PATH
-    fi
-done
-ln -f $CLICKHOUSE_ROOT_DIR/.clickhouse/$OS_URL/clickhouse $CLICKHOUSE_ROOT_DIR/.clickhouse/clickhouse
+mkdir -p $CLICKHOUSE_ROOT_DIR/.clickhouse
+ln -sf /usr/bin/clickhouse $CLICKHOUSE_ROOT_DIR/.clickhouse/clickhouse
