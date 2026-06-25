c99ac8c81420a5dd4fba3d1d592507e8a4fcb781
cosmicintruder <littlegreenbagg@googlemail.com>
2026-06-25 12:16:56 +0400

    Return Dashboard content

--- ngui/ui/src/pages/Dashboard/Dashboard.tsx
+++ ngui/ui/src/pages/Dashboard/Dashboard.tsx
@@ -1,26 +1,5 @@
-import { Stack, Typography } from "@mui/material";
-import { FormattedMessage, useIntl } from "react-intl";
-import PageContentWrapper from "components/PageContentWrapper";
-import { intl as rawIntl } from "translations/react-intl-config";
+import Dashboard from "components/Dashboard";
 
-const DashboardPage = () => {
-  const intl = useIntl();
-
-  return (
-    <PageContentWrapper>
-      <Typography variant="h6" gutterBottom>
-        Translations reactiveness
-      </Typography>
-      <Stack spacing={2}>
-        <div>Raw: {rawIntl.formatMessage({ id: "recommendations" })}</div>
-        <div>Hook: {intl.formatMessage({ id: "recommendations" })}</div>
-        <div>
-          Component:&nbsp;
-          <FormattedMessage id="recommendations" />
-        </div>
-      </Stack>
-    </PageContentWrapper>
-  );
-};
+const DashboardPage = () => <Dashboard />;
 
 export default DashboardPage;
